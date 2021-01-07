f8659d94eeaf7ee353fc9be67085c4eee6b74955
matheus6874 <matheus.scristino@gmail.com>
2021-01-06 21:33:54 -0300

    Criação da estrutura

---
+++ index.js
@@ -0,0 +1,41 @@
+class Cliente{
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo =  new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(200);
+const valorSabado = contaCorrenteRicardo.sacar(50)
+console.log(valorSabado);
+console.log(contaCorrenteRicardo);
