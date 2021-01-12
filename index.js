7a91cc50a7c8a92b10cd08509e08c76aa7180197
matheus6874 <matheus.scristino@gmail.com>
2021-01-11 23:04:59 -0300

    Finalizacao parte 2 javascript orientado a objetos

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Matheus",89934626077);
-const cliente2 = new Cliente("Alice",88822233309);
-console.log(cliente2);
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
+
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
 
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1,1001);
-contaCorrenteRicardo.depositar(500);
-
-const conta2 = new ContaCorrente(cliente2,102);
-
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
-
-console.log("valor: ", valor);
-console.log(contaCorrenteRicardo);
-console.log(ContaCorrente.numeroDeContas);
-
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
